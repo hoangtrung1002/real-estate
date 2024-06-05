55b2cfa238b8077010d3e391290ac02147294ab1
hoangtrung1002 <n.jordan1002@gmail.com>
2024-06-05 14:45:04 +0700

    setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 6666;
+
+app.listen(PORT, () => console.log(`server is running ${PORT}`));
